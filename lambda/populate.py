c3cfcfb4b8d821f1a24d33f297acfbf68e4b407c
Santiago Melo Medina <smelomedina05@gmail.com>
2024-11-01 18:16:44 -0500

    adding for populate challenges

--- lambda/populate.py
+++ lambda/populate.py
@@ -1,12 +1,33 @@
-from source.model import User, Stats, StreakDay, DayCover, Course, Path
-from source.service import UserData
+from source.model import User, Challenge
+from source.service import UserData, ChallengeData
 from source.utils import get_json_dict
 
-mock_data = get_json_dict("source/data/mock/users.json")
-users = []
 
-for user in mock_data.get("users", []):
-    users.append(User.parse_obj(user))
+def add_users():
+    mock_data = get_json_dict("source/data/mock/users.json")
+    users = []
+
+    for user in mock_data or []:
+        users.append(User.parse_obj(user))
+
+    print("USERS:", UserData().insert_user_bulk(users))
 
 
-print(UserData().insert_user_bulk(users))
+def add_challenges():
+    mock_data = get_json_dict("source/data/mock/challenges.json")
+    users = []
+
+    for user in mock_data or []:
+        users.append(Challenge.parse_obj(user))
+
+    print("CHALLENGES:", ChallengeData().insert_challenge_bulk(users))
+
+
+options = {"users": add_users, "challenges": add_challenges}
+
+if __name__ == "__main__":
+    for key in options:
+        add_data = input(f"Add {key} data (y|n): ").lower() == "y"
+
+        if add_data:
+            options.get(key)()
